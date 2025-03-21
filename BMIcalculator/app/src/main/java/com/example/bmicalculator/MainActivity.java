ab962dc356f76c6a7dc78627d51a6ac12dcb4d50
s24310-pj <s24310@pjwstk.edu.pl>
2025-03-21 15:06:51 +0100

    BMI Calculator

---
+++ BMIcalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.bmicalculator;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+
+/**
+ * Autor: Michał Kaczmarek s24310
+ * <p>
+ * Główna aktywność aplikacji Kalkulatora BMI.
+ * Pozwala użytkownikowi na wpisanie masy ciała i wzrostu,
+ * a następnie oblicza BMI i klasyfikuje kategorię BMI.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private EditText weightEditText;
+    private EditText heightEditText;
+    private TextView bmiTextView;
+    private TextView bmiCategoryTextView;
+
+    /**
+     * Metoda wywoływana przy tworzeniu aktywności.
+     * Inicjalizuje elementy interfejsu użytkownika i dodaje nasłuchiwacze zdarzeń.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        weightEditText = findViewById(R.id.weightEditText);
+        heightEditText = findViewById(R.id.heightEditText);
+        bmiTextView = findViewById(R.id.bmiTextView);
+        bmiCategoryTextView = findViewById(R.id.bmiCategoryTextView);
+
+        TextWatcher bmiTextWatcher = new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calculateBMI();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        weightEditText.addTextChangedListener(bmiTextWatcher);
+        heightEditText.addTextChangedListener(bmiTextWatcher);
+    }
+
+    /**
+     * Oblicza wartość BMI na podstawie masy ciała i wzrostu wprowadzonego przez użytkownika.
+     * Wyświetla wynik oraz kategorię BMI, zmieniając również kolor tła dla odpowiedniej klasyfikacji.
+     */
+    private void calculateBMI() {
+        String weightInput = weightEditText.getText().toString();
+        String heightInput = heightEditText.getText().toString();
+
+        if (weightInput.isEmpty() || heightInput.isEmpty()) {
+            bmiTextView.setText("");
+            bmiCategoryTextView.setText("");
+            bmiCategoryTextView.setBackgroundColor(Color.rgb(221, 221, 221));
+            return;
+        }
+
+        try {
+            double weight = Double.parseDouble(weightInput);
+            double height = Double.parseDouble(heightInput);
+
+            if (height <= 0) {
+                bmiTextView.setText(R.string.invalid_height);
+                return;
+            }
+
+            double heightMeters = height / 100.0;
+            double bmi = weight / (heightMeters * heightMeters);
+            bmiTextView.setText(String.format(Locale.getDefault(), "%.2f", bmi));
+
+            String bmiCategory;
+            int color;
+            if (bmi < 18.5) {
+                bmiCategory = "Niedowaga";
+                color = ContextCompat.getColor(this, R.color.yellow);
+            } else if (bmi < 25) {
+                bmiCategory = "Optimum";
+                color = ContextCompat.getColor(this, R.color.green);
+            } else if (bmi < 30) {
+                bmiCategory = "Nadwaga";
+                color = ContextCompat.getColor(this, R.color.orange);
+            } else {
+                bmiCategory = "Otyłość";
+                color = ContextCompat.getColor(this, R.color.red);
+            }
+            bmiCategoryTextView.setText(bmiCategory);
+            bmiCategoryTextView.setBackgroundColor(color);
+
+        } catch (NumberFormatException e) {
+            bmiTextView.setText(R.string.invalid_mass);
+            bmiCategoryTextView.setText("");
+            bmiCategoryTextView.setBackgroundColor(Color.rgb(221, 221, 221));
+        }
+    }
+}
